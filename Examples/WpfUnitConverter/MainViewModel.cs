b9a8c057f034ebee936f1a444b95edc723dcdc9d
StormDotNetAdmin <mail.stormdotnet@gmail.com>
2019-12-10 16:16:13 +0100

    Add WpfUnitConverter demo

---
+++ Examples/WpfUnitConverter/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿// Storm.NET - Simple Topologically Ordered Reactive Model
+// Copyright © 2019 Storm.NET. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace WpfUnitConverter
+{
+    using System;
+    using System.Globalization;
+    using StormDotNet;
+
+    public class MainViewModel
+    {
+        private const double MeterByInch = 0.0254;
+
+        public MainViewModel()
+        {
+            InchIo = new WpfStormIo<string>();
+            MeterIo = new WpfStormIo<string>();
+
+            var modelFromInch = Storm.Func.Create(InchIo.Input, s => double.Parse(s) * MeterByInch);
+            var modelFromMeter = Storm.Func.Create(MeterIo.Input, double.Parse);
+
+            var model = Storm.Func.FromStates.Create(modelFromMeter, modelFromInch, GetModelValue);
+
+            var inchFromModel = Storm.Func.Create(model, v => Convert.ToString(v / MeterByInch, CultureInfo.CurrentCulture));
+            var meterFromModel = Storm.Func.Create(model, Convert.ToString);
+
+            InchIo.Update.Connect(inchFromModel);
+            MeterIo.Update.Connect(meterFromModel);
+        }
+
+        private static double GetModelValue(StormFuncInput<double> meterValue, StormFuncInput<double> inchValue)
+        {
+            return (meterValue.State, inchValue.State) switch
+            {
+                (_, EStormFuncInputState.NotVisited) => meterValue.Content.GetValueOr(double.NaN),
+                (EStormFuncInputState.NotVisited, _) => inchValue.Content.GetValueOr(double.NaN),
+                _ => double.NaN
+            };
+        }
+
+        public WpfStormIo<string> InchIo { get; }
+        public WpfStormIo<string> MeterIo { get; }
+    }
+}
---
+++ Examples/WpfUnitConverter/WpfStorm.cs
@@ -0,0 +1,94 @@
+﻿// Storm.NET - Simple Topologically Ordered Reactive Model
+// Copyright © 2019 Storm.NET. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace WpfUnitConverter
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using StormDotNet;
+
+    public class WpfStormBase : INotifyPropertyChanged
+    {
+        protected void Update<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value))
+                return;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class WpfStorm<T> : WpfStormBase
+    {
+        private T _value;
+
+        public T Value
+        {
+            get => _value;
+            set => Update(ref _value, value);
+        }
+    }
+
+    public class WpfStormIo<T> : INotifyPropertyChanged
+    {
+        private readonly IStorm<T> _output;
+
+        public WpfStormIo()
+        {
+            Input = Storm.Input.Create<T>();
+            Update = Storm.Socket.Create<T>();
+
+            _output = Storm.Func.FromStates.Create(Input, Update, Resolve);
+            _output.OnVisit += OutputOnVisit;
+        }
+
+        public IStormInput<T> Input { get; }
+
+        public IStormSocket<T> Update { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public T Value
+        {
+            get => _output.TryGetValue(out var value) ? value : default;
+            set => Input.SetValue(value);
+        }
+
+        private void OutputOnVisit(IStormToken token, EStormVisitType visitType)
+        {
+            if (visitType == EStormVisitType.LeaveChanged)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+            }
+        }
+
+        private static T Resolve(StormFuncInput<T> input, StormFuncInput<T> update)
+        {
+            return (input.State, update.State) switch
+            {
+                (EStormFuncInputState.NotVisited, _) => update.Content.GetValueOr(default),
+                _ => input.Content.GetValueOr(default)
+            };
+        }
+    }
+}
